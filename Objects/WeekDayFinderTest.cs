2ee7d34773472c6b0cd341d4d0ee4794a5ac2cfd
Andrew Cho <cho-andrew@hotmail.com and trashmoldwilliams@gmail.com>
2016-02-18 11:46:47 -0800

    add test for accomodation of differing month day amounts.

--- Objects/WeekDayFinderTest.cs
+++ Objects/WeekDayFinderTest.cs
@@ -16,5 +16,13 @@ namespace WeekDayFinderTest
       double day = 2;
       Assert.Equal("Thursday", WeekDayFinder.FindWeekDay(month, day, year));
     }
+    [Fact]
+    public void IsDate_forDateBeing03011800_()
+    {
+      double year = 1800;
+      double month = 3;
+      double day = 1;
+      Assert.Equal("Saturday", WeekDayFinder.FindWeekDay(month, day, year));
+    }
   }
 }
--- Objects/WeekdayFinderObjects.cs
+++ Objects/WeekdayFinderObjects.cs
@@ -2,26 +2,27 @@ using System;
 using System.Collections.Generic;
 namespace WeekDayFinders
 {
-    public class WeekDayFinder
+  public class WeekDayFinder
   {
       private string _day;
       private string _month;
       private string _year;
-//constructor//
 
+    //constructor//
     public WeekDayFinder()
     {
 
     }
 
     public static string FindWeekDay(double month, double day, double year)
-     {
-       var daysSinceAnchor = ((year - 1800) * 365) + ((month - 1) * 30) + day;
+    {
+       double daysSinceAnchor = ((year - 1800) * 365) + WeekDayFinder.findDaysAMonth(month - 1) + day;
 
        double weekdayNumber = daysSinceAnchor % 7;
+       Console.WriteLine("Weekday Number: " + weekdayNumber);
 
        Dictionary<double, string> testWeek = new Dictionary<double, string> {};
-       testWeek.Add(1, "Wedensday");
+       testWeek.Add(1, "Wednesday");
        testWeek.Add(2, "Thursday");
        testWeek.Add(3, "Friday");
        testWeek.Add(4, "Saturday");
@@ -29,20 +30,47 @@ namespace WeekDayFinders
        testWeek.Add(6, "Monday");
        testWeek.Add(7, "Tuesday");
 
-       string output = "";
+       string output = "FAILURE";
 
        foreach (KeyValuePair<double, string> test in testWeek)
        {
          if( weekdayNumber == test.Key)
          {
+           Console.WriteLine("Into loop!");
            output = test.Value;
          }
        }
 
        return output;
-     }
+    }
 
-// getters and setters for properties//
+    public static double findDaysAMonth(double month)
+    {
+      Dictionary<double, double> testMonth = new Dictionary<double, double> {};
+      testMonth.Add(1, 31);
+      testMonth.Add(2, 28);
+      testMonth.Add(3, 31);
+      testMonth.Add(4, 30);
+      testMonth.Add(5, 31);
+      testMonth.Add(6, 30);
+      testMonth.Add(7, 31);
+      testMonth.Add(8, 31);
+      testMonth.Add(9, 30);
+      testMonth.Add(10, 31);
+      testMonth.Add(11, 30);
+      testMonth.Add(12, 31);
 
+      double output = 0;
+
+      foreach ( KeyValuePair<double, double> test in testMonth)
+      {
+        if( month >= test.Key)
+        {
+          output += test.Value;
+        }
+      }
+      return output;
+
+    }
   }
 }
